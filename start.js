cec61115fc258440a62796697791b206c81bc5a5
Robert Salmon <robertsalmon@Roberts-MacBook-Pro.local>
2022-07-07 09:33:48 -0400

    initial commit

---
+++ start.js
@@ -0,0 +1,11 @@
+// var numArray = [10, 5, 17, 50, 4, 25, 71, 78, 39, 85, 65, 2, 7, 5, 53]
+
+// for(i = 0; i < numArray.length; i++) {
+//     numArray.sort()
+//     console.log(numArray);
+// };
+
+
+
+
+   
